eff9576558cd3eace2f58014fb9728b7343b3dda
Ignacio Davicino <ignaciodavicino@gmail.com>
2022-08-24 09:52:07 -0300

    changes to css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,28 @@
+
+
+
 let styles1 = {
-    display: "inline",
-    color:"orange",
+    display: "flex",
+    flexDirection: "column",
+    color:"black",
+    justifyContent:"center",
+    textAlign:"center",
+    background: "hsla(72, 100%, 47%, 1)",
+    background:" linear-gradient(135deg, hsla(72, 100%, 47%, 1) 10%, hsla(246, 100%, 39%, 1) 100%)",
+    background: "-moz-linear-gradient(135deg, hsla(72, 100%, 47%, 1) 10%, hsla(246, 100%, 39%, 1) 100%)",
+    background: "-webkit-linear-gradient(135deg, hsla(72, 100%, 47%, 1) 10%, hsla(246, 100%, 39%, 1) 100%)",
 };
 
 let styles2 = {
+    display:"flex",
+    flexDirection:"column",
     fontSize: "30px",
-    backgroundColor: "black",
-    listStyle:"none"
+    listStyle:"none",
+    alignItems: "center",
+    textAlign: "center",
+    textTransform: "upperCase"
 }
+
 
 
 export function NavBar () {
@@ -18,14 +33,14 @@ export function NavBar () {
             </h2>
             <ul style={styles2}>
             <li>
-            Nuestros Bajos
+            Our Craft
 
             </li>
             <li>
-            Inicio
+            Home
 
             </li>
-            Contactanos
+            Contact Us
             <li>
 
             </li>
